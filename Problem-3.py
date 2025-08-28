44274335afda83c53de4f7c19ec4c85b503ede23
amitcoderhub <amitchauhan6599@gmail.com>
2025-08-28 23:14:27 +0530

    push

---
+++ Problem-3.py
@@ -0,0 +1,9 @@
+# WAP to print give string are anagram or not
+
+
+s1 = input("Enter a string: ")
+s2 = input("Enter a string: ")
+if sorted(s1)==sorted(s2):
+    print("anagram")
+else:
+    print("not")
