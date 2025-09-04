10d8c6e84557ebbd604e6fa5f148221e089a3f58
amitcoderhub <amitchauhan6599@gmail.com>
2025-09-04 22:41:57 +0530

    emirp

---
+++ Functions/F-emirp.py
@@ -0,0 +1,28 @@
+# wap to print EMIRP num in give a range using function
+
+
+
+def isPrime(n):
+    if n>1:
+        for i in range(2,n//2+1):
+            if n%i==0:
+                return False
+        else:
+            return True
+    else:
+        return False
+def rev(n):
+    rev = 0
+    while n >0:
+        rem = n%10
+        n//=10
+        rev = rev*10+rem
+    return rev
+        
+def EmirpNum(ll,ul):
+    for n in range(ll,ul+1):
+        r= rev(n)
+        if isPrime(n) and r != n and isPrime(r):
+            print(n)
+
+EmirpNum(1,30)       
