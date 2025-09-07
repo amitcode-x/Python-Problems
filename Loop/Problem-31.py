035034fc7b492f8a3fcccf116606a0d0ec8ed529
amitcoderhub <amitchauhan6599@gmail.com>
2025-09-07 18:51:07 +0530

    L-31

---
+++ Loop/Problem-31.py
@@ -0,0 +1,31 @@
+# Wap to print only char at odd index position
+
+
+
+s = input('Enter a string :')
+for ip in range(1,len(s),2):
+        print(s[ip])
+
+
+
+
+
+
+      
+
+
+
+
+
+
+
+    
+
+
+        
+
+    
+    
+
+
+
