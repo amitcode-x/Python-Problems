44d04acf8ee2e83c156e8411a58b49ebb4d6cf3a
amitcoderhub <amitchauhan6599@gmail.com>
2025-09-08 22:10:51 +0530

    duck

---
+++ Functions/F-duck.py
@@ -0,0 +1,17 @@
+def isDuck(n):
+    s = str(n)
+ 
+    if s.startswith("0"):
+        return False
+
+    return "0" in s
+
+
+def DuckNum(LL, UL):
+    for n in range(LL, UL + 1):
+        if isDuck(n):
+            print(n)
+
+
+
+DuckNum(1, 200)
