c6f0e119ef0b9dae53507f672b541a7fe666c924
amitcoderhub <amitchauhan6599@gmail.com>
2025-09-09 22:00:04 +0530

    R-07

---
+++ Recursion/R-07.py
@@ -0,0 +1,11 @@
+# WAP to check whether a string is palindrome using recursion.
+
+def is_palindrome(s):
+    if len(s) <= 1:
+        return True
+    else:
+        return (s[0] == s[-1]) and is_palindrome(s[1:-1])
+if is_palindrome('MADAM'):
+    print("Palindrome")
+else:
+    print("Not Palindrome")
