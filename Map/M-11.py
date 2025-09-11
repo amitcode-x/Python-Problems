c51b1fd5ddfb9d69430e36f85d180f52b6cfe39e
amitcoderhub <amitchauhan6599@gmail.com>
2025-09-11 14:49:57 +0530

    M-11

--- Map/M-11.py
+++ Map/M-11.py
@@ -1,6 +1,10 @@
 # i/p : 12 45 67 34 66
 # o/p : [12,45,67,34,66]
 
+# taking space seprate integer values as input
+
+
+print(list(map(lambda a: int(a),input("Enter a num: ").split())))
 
 
 l = list(map(int,input("enter a num:").split()))
