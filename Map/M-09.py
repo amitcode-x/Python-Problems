ee38f6f939754c37dbfcd5205dd05ec266abcdbb
amitcoderhub <amitchauhan6599@gmail.com>
2025-09-11 14:40:57 +0530

    M-09

---
+++ Map/M-09.py
@@ -0,0 +1,27 @@
+# i/p = ['11','22','30','13']
+# o/p = [11,22,30,13] using normal function
+
+
+
+def ip(l):
+    return int(l)
+print(list(map(ip,['11','22','30','13'])));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
