26f7f9b8a43923c63d401172ae7d466b491e1540
amitcoderhub <amitchauhan6599@gmail.com>
2025-09-11 14:51:14 +0530

    M-11

--- Map/M-11.py
+++ Map/M-11.py
@@ -4,8 +4,8 @@
 # taking space seprate integer values as input
 
 
-print(list(map(lambda a: int(a),input("Enter a num: ").split())))
 
 
 l = list(map(int,input("enter a num:").split()))
 print(l)
+
