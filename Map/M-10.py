1e71aeb8017f98765f187366b83eced0041a58d3
amitcoderhub <amitchauhan6599@gmail.com>
2025-09-16 21:17:05 +0530

    M-10

--- Map/M-10.py
+++ Map/M-10.py
@@ -1,5 +1,5 @@
 # i/p = ['11','22','30','13']
-# o/p = [11,22,30,13] map using lambda  function
+# o/p = [11,22,30,13] map using lambda function
 
 
 print(list(map(lambda l: int(l),['11','22','30','13'])))
