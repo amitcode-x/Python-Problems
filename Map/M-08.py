ac28d342d1e84e7a6285dc7c0bcd2f47c792ee77
amitcoderhub <amitchauhan6599@gmail.com>
2025-09-17 22:39:34 +0530

    M-08

--- Map/M-08.py
+++ Map/M-08.py
@@ -1,4 +1,4 @@
 # i/p = [11,22,30,13]
-# o/p = [false,true,true,false] using lambda function
+# o/p = [false,true,true,false] using  lambda function
 
 print(list(map(lambda l:l%2==0,[11,22,30,13])))
