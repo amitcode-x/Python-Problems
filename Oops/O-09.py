b27084526c3e734af781ceca7ae28ef00eb6f92a
amitcoderhub <amitchauhan6599@gmail.com>
2025-09-18 12:13:09 +0530

    O-09

---
+++ Oops/O-09.py
@@ -0,0 +1,16 @@
+#creating new generic properties by using object
+#syntax:   ObjectName.classVariablename = value
+
+class Bank:
+    # ---- Class Variables ----
+    bank_name = "State Bank of India"
+    bank_branch = "Marathahalli, Bangalore"
+    bank_roi = 5   # Rate of Interest
+
+# ---- Objects ----
+amit = Bank()
+sumit = Bank()
+
+amit.bank_mobile = 1234
+
+#we can create but its generic approch so its not possible
