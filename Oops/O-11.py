8ac15c5ec639c623f7e9bfc4b26aa57e02182ccf
amitcoderhub <amitchauhan6599@gmail.com>
2025-09-18 12:40:32 +0530

    O-11

---
+++ Oops/O-11.py
@@ -0,0 +1,14 @@
+#  
+class Bank:
+    bank_name = 'sbi'
+    bank_branch = 'marathahli'
+    bank_roi = 5
+    def __init__(self,cn,ac,b):
+        print(self)
+        self.cname= cn
+        self.account = ac
+        self.balance = b
+amit = Bank('Amit chauhan',1234,100)
+print("Customer Name:", amit.cname)
+print("Account Number:", amit.account)
+print("Balance:", amit.balance)
