fe979ee51e1a4be3aa4e3eaeeeba7bfbb957b98d
amitcoderhub <amitchauhan6599@gmail.com>
2025-09-18 11:49:20 +0530

    O-02

---
+++ Oops/O-02.py
@@ -0,0 +1,17 @@
+class Bank:
+    # ---- Class Variables ----
+    bank_name = "State Bank of India"
+    bank_branch = "Marathahalli, Bangalore"
+    bank_roi = 5   # Rate of Interest
+
+
+# ---- Objects ----
+amit = Bank()
+sumit = Bank()
+
+print(amit.bank_branch)
+print(amit.bank_name)
+print(amit.bank_roi)
+
+
+
