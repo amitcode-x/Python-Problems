ea6e5ef6775397e040102f580b5f4b7196904e59
amitcoderhub <amitchauhan6599@gmail.com>
2025-09-19 21:58:44 +0530

    O-12

--- Oops/O-12.py
+++ Oops/O-12.py
@@ -8,7 +8,7 @@ class Bank:
         self.balance=b
     def Customer_details(self):
         print('customer name :',self.cname)
-        print('customer acount no :',self.acount)
+        print('customer acount :',self.acount)
         print('customer name :',self.balance)
     def Withdraw(self):
         amount= int(input('Enter the amount :'))
