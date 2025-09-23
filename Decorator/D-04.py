3368f21f6c2537e281229a35874319c79595a780
amitcoderhub <amitchauhan6599@gmail.com>
2025-09-23 11:26:58 +0530

    D-04

---
+++ Decorator/D-04.py
@@ -0,0 +1,21 @@
+def my_decorator(func):
+    print('outer start')
+    def wrapper():
+        print("Before the function runs")
+        func()
+        print("After the function runs")
+    print('outer end')
+    return wrapper
+
+
+
+
+
+@my_decorator
+#say_hello = my_decorator(say_hello)
+
+def say_hello():
+    print("Hello, world!")
+
+
+say_hello()
