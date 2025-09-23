c865294f3b3b1c58292106d00f374316035c38b5
amitcoderhub <amitchauhan6599@gmail.com>
2025-09-23 23:29:08 +0530

    O-14

---
+++ Oops/O-14.py
@@ -0,0 +1,15 @@
+class Bank:
+    bank_roi = 5
+    @classmethod
+    def modify_roi(cls):
+        print(cls)
+        cls.bank_roi=4
+        print('modified')
+
+amit=Bank()
+sumit=Bank()
+#Object method sirf object se call hota hai class se nhi
+Bank.modify_roi()
+print(sumit.bank_roi)
+print(amit.bank_roi)
+print(Bank.bank_roi)
