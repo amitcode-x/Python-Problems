cacd1d8d997ca7645d8f620de346c99299bb9c76
amitcoderhub <amitchauhan6599@gmail.com>
2025-09-28 00:03:23 +0530

    anagram

---
+++ Python Interview/Anagram1.py
@@ -0,0 +1,14 @@
+from collections import Counter
+
+
+def is_anagram(s1, s2):
+    s1 = s1.replace(" ", "").lower()
+    s2 = s2.replace(" ", "").lower()
+    
+    return Counter(s1) == Counter(s2)
+s1=input('Enter s1 string:')
+s2=input('Enter s2 string:')
+if is_anagram(s1, s2):
+    print("anagram")
+else:
+    print("not")
