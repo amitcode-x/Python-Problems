8ea93788b135edc5d94eda22edcb3bacb627a9be
amitcoderhub <amitchauhan6599@gmail.com>
2025-09-28 21:57:13 +0530

    frequency

--- Python Interview/frequence.py
+++ Python Interview/frequence.py
@@ -2,3 +2,16 @@ from collections import Counter
 s = input('Enter a string :')
 c =Counter(s)
 print(c)
+
+# Frequency of characters in a string
+s = input("Enter a string: ")
+freq = {}
+
+for ch in s:
+    if ch in freq:
+        freq[ch] += 1
+    else:
+        freq[ch] = 1
+
+for ch in freq:
+    print(f"{ch}: {freq[ch]}")
