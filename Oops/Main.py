c8496bd7be4462f724a073187af14cb8bff9d988
amitcoderhub <amitchauhan6599@gmail.com>
2025-10-02 22:58:47 +0530

    main

--- Oops/Main.py
+++ Oops/Main.py
@@ -284,7 +284,7 @@ class HeartDrawingApp:
         self.start_btn.config(state=tk.NORMAL)
         self.pause_btn.config(state=tk.DISABLED)
         self.status_label.config(text="Ready to draw!")
-    
+     
     def update_speed(self, value): 
         """Update drawing speed"""  
         self.speed = float(value) 
