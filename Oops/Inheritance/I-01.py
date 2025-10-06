3a7168ee918e35627f64cb1e9f2417cedeb42b7f
amitcoderhub <amitchauhan6599@gmail.com>
2025-10-06 11:29:26 +0530

    I-01

---
+++ Oops/Inheritance/I-01.py
@@ -0,0 +1,11 @@
+class Father:
+    bike= 'chetak'
+    car ='nano'
+    money=100000
+mother = Father()
+print(mother.money)
+class Son(Father):
+    bike='himalayan'
+    house='2bhk'
+wife=Son()
+print(wife.money)
