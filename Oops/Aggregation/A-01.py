43bebda1e3aafa7035f89dfaf4c3d86f81b2673e
amitcoderhub <amitchauhan6599@gmail.com>
2025-10-06 11:27:35 +0530

    A

---
+++ Oops/Aggregation/A-01.py
@@ -0,0 +1,38 @@
+
+class Address:
+
+    def __init__(self,c,s,co):
+        self.city=c
+        self.state=s
+        self.country=co
+    def display_add(self):
+        print(f' city name is : {self.city}')
+        print(f' state name is : {self.state}')
+        print(f' country name is : {self.country}')
+
+Banglore=Address('Banglore','Karnatka','India')
+
+##Object method sirf object se call hota hai class se nhi ,ager hum class se call krenge to aese likhenge classname.objectmethodname(object)
+
+Address.display_add(Banglore)
+
+#ager hum class se call krenge to aese likhenge classname.objectmethodname(object)
+
+Banglore.display_add()
+
+
+class Student:
+    
+    def __init__(self,sN,sAge,sC,aD):
+        self.stname=sN
+        self.sAge=sAge
+        self.sClass=sC
+        self.Address=aD
+    def student_details(self):
+        print(f'student name is : {self.stname} ')
+        print(f'student Age is : {self.sAge} ')
+        print(f'student Class is : {self.sClass} ')
+        self.Address.display_add()
+
+amit=Student('amit',22,'python',Banglore)
+amit.student_details()
