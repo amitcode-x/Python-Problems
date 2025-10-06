16b296b65b73dbeb7a0d5a32fd6dd0a953400235
amitcoderhub <amitchauhan6599@gmail.com>
2025-10-06 12:44:19 +0530

    I-06

--- Oops/Inheritance/I-06.py
+++ Oops/Inheritance/I-06.py
@@ -15,7 +15,7 @@ class Bank_v1:
     def get_int_value(): 
         iv = int(input())  
         return iv 
-    def withdraw(self): 
+    def withdraw(self):  
         print('Enter the withdraw amounts :')
         amount = self.get_int_value()
         if amount <= self.cbalance:
