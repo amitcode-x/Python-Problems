a85c2e387ac99e1f341d684b7ec4db14c56e2032
amitcoderhub <amitchauhan6599@gmail.com>
2025-10-06 12:51:34 +0530

    I-06

--- Oops/Inheritance/I-06.py
+++ Oops/Inheritance/I-06.py
@@ -10,7 +10,7 @@ class Bank_v1:
     def customer_details(self): 
         print(f'name of customer is {self.cname}')
         print(f'account of customer is {self.caccount}')
-        print(f'balance of customer is {self.cbalance}')
+        print(f'balance of customer is {self.cbalance}') 
     @staticmethod  
     def get_int_value():       
         iv = int(input())    
