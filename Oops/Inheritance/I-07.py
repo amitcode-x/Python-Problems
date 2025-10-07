cebcaed01d475a9587dbc25118ba2b5885977cb3
amitcoderhub <amitchauhan6599@gmail.com>
2025-10-07 14:18:22 +0530

    I-07

--- Oops/Inheritance/I-07.py
+++ Oops/Inheritance/I-07.py
@@ -37,7 +37,7 @@ class Bank_v1:
         nroi = cls.get_int_value()
         cls.bank_roi = nroi
         print('roi is changed')
- 
+  
    
 class Bank_v2(Bank_v1):   
     bank_branch='banglore'     
