384abcf2005ae4dacb932614b96f500a2a40ed0b
amitcoderhub <amitchauhan6599@gmail.com>
2025-10-07 14:13:52 +0530

    I-07

--- Oops/Inheritance/I-07.py
+++ Oops/Inheritance/I-07.py
@@ -52,7 +52,7 @@ class Bank_v2(Bank_v1):
         print(f'mobile num of customer {self.cmobile}')
     def withdraw(self):
         print('Enter the pin ') 
-        pin = self.get_int_value()
+        pin = self.get_int_value() 
         if pin == self.cpin: 
             print('PIN verified successfully!')
             super().withdraw()
