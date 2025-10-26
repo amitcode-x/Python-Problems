476c2022079f43878faf6c51857ef55b6b6c83c2
amitcoderhub <amitchauhan6599@gmail.com>
2025-10-26 21:28:12 +0530

    IT-05

---
+++ Iterator/IT-05.py
@@ -0,0 +1,26 @@
+# bysir fiboIterator
+
+
+
+
+
+
+class FeboIterator():
+    def __init__(self,fv,sv,n):
+        self.fv = fv
+        self.sv = sv
+        self.n = n
+
+    def __iter__(self):
+        self.i = 1
+        return self
+    def __next__(self):
+        if self.i<=self.n:
+            self.i+=1
+            res= self.fv
+            self.fv,self.sv= self.sv,self.fv+self.n
+            return res
+        raise StopIteration
+FIO = FeboIterator(20,22,10)
+for i in FIO:
+    print(i)
