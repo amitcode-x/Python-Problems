47a0ebd97d24738625affd8ea5267c5f627a59c5
amitcoderhub <amitchauhan6599@gmail.com>
2025-10-27 13:09:36 +0530

    Tc-01

---
+++ Comprehensions/Tuple Comprehension/Tc.02.py
@@ -0,0 +1,12 @@
+# synatx:
+
+# ListC = [value for value in l]
+# Dictc = {i:(performing operation on i) for i in l}
+
+# Dictc = {key: value for key in gdi}
+
+
+#{1:1,2:4,3:9,4:16}
+
+dictval = {i:i**2 for i in range(1,11)}
+print(dictval)
