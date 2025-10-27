e28ace722de5515b805fd3ed883e50b68ce29d6b
amitcoderhub <amitchauhan6599@gmail.com>
2025-10-27 12:27:52 +0530

    Lc-01

---
+++ Comprehensions/List Comprehension/Lc-01.py
@@ -0,0 +1,14 @@
+# Adding the num into list by using Normal Approch
+
+L =[]
+for i in range(1,11):
+    L.append(i)
+print(L)
+
+# Adding the num into list by usingList Comprehension
+
+# syntax
+# variablename = [value for loop]
+
+L = [j for j in range(1,11)]
+print(L)
