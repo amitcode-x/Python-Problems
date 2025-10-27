1d939266cd17db7729cd77ec7abe21e1b60c425f
amitcoderhub <amitchauhan6599@gmail.com>
2025-10-27 12:31:48 +0530

    Lc-08

---
+++ Comprehensions/List Comprehension/Lc-08.py
@@ -0,0 +1,9 @@
+
+# Sytanx List comprehension with value forloop if condition and else 
+# syntax:
+
+# variablename = [value if consition else forloop]
+
+
+L = [i if i>0  else 0 for i in [1,2,3,-88,100,-99]]
+print(L)
